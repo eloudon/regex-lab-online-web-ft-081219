0226fdec164743e51bd58f88db5b0af6abaa9d5f
Eric Loudon <loudon.eric@gmail.com>
2019-08-16 13:39:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-if word.match(/^[AEIOU] | ^[aeiou]/)
-  true
-else
-  false
-end
+  if word.match(/^[AEIOU]|^[aeiou]/)
+     true
+   else
+     false
+   end
 
 end
 
