a9d3cca988c3e029af6d79770d2ecc1c72136567
Eric Loudon <loudon.eric@gmail.com>
2019-08-16 13:40:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   first_letter = text.scan(/\w+/)[0]
     if (text.scan(/\./)) && (first_letter == first_letter.upcase)
       true
-    else  
+    else
       false
     end
 end
@@ -28,7 +28,7 @@ end
 def valid_phone_number?(phone)
   if text.match(/^[A-Z]/) && text.match(/[\.]$/)
       true
-    else 
+    else
       false
     end
 end
